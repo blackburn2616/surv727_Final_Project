d16ab811652d1e49f541e9181d50b3503b3e12d7
Nicole Blackburn <nblackbu@umd.edu>
2025-12-03 16:19:38 -0500

    comment_pulls script

--- comment_pulls.R
+++ comment_pulls.R
@@ -1,20 +1,14 @@
+library(RedditExtractoR)
+library(tidyverse)
 
-# We search for submissions which contain the word 'validated' to subreddit 'narcolepsy'. 
-#From the resulting df, we get the URLs pointing to each submission. 
-validated_urls <- find_thread_urls(keywords = "validated",
-                                   subreddit = "narcolepsy",
-                                   sort_by = "new")
+data_raw <- read.csv("r_narc_nov11.csv")
 
-head(validated_urls$url)
+com_2 <- get_thread_content(data_raw$url)
 
-#We next get the comments to the submissions that we pulled above using the URLs of the submissions as reference.
+threads2 <- com_2$threads
 
-validated_content <- get_thread_content(urls = validated_urls$url[1:5])
+comments2 <- com_2$comments
 
-#Using post_date (submission) and comm_date (comment), we can select submissions and comments 
-#based on the time they were posted. Using link, we could try to obtain additional information 
-#about submission content (e.g., link may post to a news article). Structure allows us to obtain 
-#an overview of the comment structure. Most interestingly, comment gives us the content of the comment.
+write.csv(comments2, "narc_comments.csv")
 
-
-head(validated_content$comments)
+write.csv(threads2, "narc_threads.csv")
