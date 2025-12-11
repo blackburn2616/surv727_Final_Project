8718f54ca256488bd144ace144b1593050259a71
Nicole Blackburn <nblackbu@umd.edu>
2025-12-11 13:35:32 -0500

    Final Project Commits

---
+++ data/reddit_pulls.R
@@ -0,0 +1,35 @@
+library(tidytext)
+library(RedditExtractoR)
+
+##Posts
+#pull posts from reddit. I did this on November 11.
+posts <- find_thread_urls(subreddit = "narcolepsy",
+                          sort_by = "new",
+                          period = "month")
+write.csv(posts, "r_narc_nov11.csv")
+
+#Add doc_id column
+posts$doc_id <- seq_len(nrow(posts))
+write.csv(posts, "r_narc_nov11.csv", row.names = FALSE)
+
+##Comments
+com_2 <- get_thread_content(posts$url) #this kicked me at 412 posts
+
+threads2 <- com_2$threads
+write.csv(threads2, "narc_threads.csv")
+
+comments2 <- com_2$comments
+write.csv(comments2, "narc_comments.csv")
+
+
+
+
+
+
+
+
+
+#New csv for only the first 412 posts to match comments
+posts <- posts |>
+  slice(1:412)
+write.csv(posts, "narc_posts.csv")
